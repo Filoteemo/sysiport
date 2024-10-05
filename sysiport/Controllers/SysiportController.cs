b9985524b770954f4575a712db60026613e2ea16
Sindre Olsen <sindre-olsen@outlook.com>
2024-10-05 17:24:57 +0200

    added new project

---
+++ sysiport/Controllers/SysiportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sysiport.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SysiportController : ControllerBase
+    {
+
+    }
+}
