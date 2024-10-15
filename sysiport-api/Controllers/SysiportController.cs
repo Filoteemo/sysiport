9956926222dcb53044f8af5d8a8052889e983b62
Sindre Olsen <sindre-olsen@outlook.com>
2024-10-15 23:48:41 +0200

    Revert "adding moldes"
    This reverts commit 8428531635030b68a0a2afe47bcaa6b25e53ef74.

---
+++ sysiport-api/Controllers/SysiportController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sysiport_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SysiportController : ControllerBase
+    {
+    }
+}
